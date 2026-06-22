f839a37da8817ae76384cb81f1a9ccaf239ee755
umarxon2000 <anasxonovumarxon2000@gmail.com>
2026-06-22 17:49:16 +0500

    barcha sahifalar tipizatsiya qilindi

--- lib/calculator.ts
+++ lib/calculator.ts
@@ -1,13 +1,13 @@
 import { Product } from "@/types/product";
 
 export const calculateQuantity = (m2: number, product: Product) => {
-  const areaPerPiece = (product.width * product.height) / 1000000; // m2 ga o'tkazish
+  const areaPerPiece = (product.width * product.thickness) / 1000000; // m2 ga o'tkazish
   const piecesNeeded = Math.ceil(m2 / areaPerPiece);
-  const packagesNeeded = Math.ceil(piecesNeeded / product.inPackage);
+  const packagesNeeded = Math.ceil(piecesNeeded / product.in_package);
 
   return {
     pieces: piecesNeeded,
     packages: packagesNeeded,
-    totalPrice: piecesNeeded * product.pricePerUnit
+    totalPrice: piecesNeeded * product.price_per_unit
   };
 };
